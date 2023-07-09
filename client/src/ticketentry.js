f0136982f6a770368b37ec55a0e1099426cf34d4
Tolga Simsek <tol_simsek@live.de>
2023-07-09 17:19:48 +0200

    filter + render

--- client/src/ticketentry.js
+++ client/src/ticketentry.js
@@ -2,34 +2,30 @@ import React, { useEffect, useState } from "react";
 
 const EntryComponent = ({ entry }) => {
     // Render the entry's data here
-    return <div>{entry.title}</div>;
+    return <div>{entry.COMMON[0]}</div>;
 };
 
 const Ticketfetch = () => {
     const [data, setData] = useState([]);
 
     useEffect(() => {
-        fetch("/api/xml") // Replace with your JSON endpoint
+        fetch("/api/xml") // Replace with JSON endpoint
             .then((response) => response.json())
             .then((jsonData) => {
                 console.log(jsonData.CATALOG.PLANT);
-                setData(jsonData.CATALOG.PLANT); // Adjust the property path to match your JSON structure
+                setData(jsonData.CATALOG.PLANT); // Adjust the property path to match JSON structure
             })
             .catch((error) => console.error("Error:", error));
     }, []);
 
+    const filteredData = data.filter((entry) => entry.ZONE[0] === "4"); // Filter the data array based on the condition
     return (
         <div>
-            {data.map((entry, index) => (
+            {filteredData.map((entry, index) => (
                 <EntryComponent key={index} entry={entry} />
             ))}
         </div>
     );
 };
-
-// map through entries & filter
-// create component for each suiting entry
-// second function to implement another filter
-//push p
 
 export default Ticketfetch;
